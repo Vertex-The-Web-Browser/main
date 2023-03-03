e017f7eb8bdc9e6616c061501af02c42797e9cfd
Vikas Keshavamurthy Bhat <Vikas Keshavamurthy Bhat>
2023-03-03 12:07:57 +0530

    Made changes to tab and window files

--- window.py
+++ window.py
@@ -1,6 +1,9 @@
 import gi
 gi.require_version("Gtk", "3.0")
+gi.require_version("Pango", "1.0")
+
 from gi.repository import Gtk
+from gi.repository import Pango
 
 from tab import Tab
 
@@ -17,13 +20,31 @@ class Window:
         self.browser_window_notebook = Gtk.Notebook()
         self.browser_window.add(self.browser_window_notebook)
 
-        self.tab_list = []       
-
 
     def add_tab_to_notebook(self):
+        '''
+        Creates a new tab object, and adds the tab as a page in the notebook
+        Sets the tab to be reorderable
+        Creates a label object for labelling a page, and passes it to 
+        tab
+        '''
 
-        new_tab = Tab(self.browser_window_notebook)
-        self.tab_list.append(new_tab)
+        # Creates a new laebl with text Loading
+        tab_label = Gtk.Label("Loading")
+
+        # Sets width of label to MAX_WIDTH chars, so that all pages have same width
+        tab_label.set_width_chars(20)
+
+        # Adds ellipses to the end of label if text is too long
+        tab_label.set_ellipsize(Pango.EllipsizeMode.END)
+
+        new_tab = Tab(tab_label)
+
+        self.browser_window_notebook.append_page(new_tab.get_container())
+
+        self.browser_window_notebook.set_tab_label(new_tab.get_container(), tab_label)
+
+        self.browser_window_notebook.set_tab_reorderable(new_tab.get_container(), True)
 
     
     def start_event_loop(self):
